4f9b7925883ea69ac67f87e55099a35f1bfe874d
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2022-01-29 18:04:22 +0100

    Fix broken service-worker.js path

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('./service-worker.js')
+        .register('../service-worker.js')
         .then(function(registration) {
             console.log("Service worker registered");
         })
