8b0e569f0bac119dd493079b166bc8f1b7b6bae8
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2024-02-18 20:41:52 +0100

    Add Hero.svelte

---
+++ src/components/Hero.svelte
@@ -0,0 +1,13 @@
+<section id="Hero">
+	<div class="relative px-6 pt-24 pb-8 bg-green-100">
+		<div class="mx-auto max-w-2xl py-32">
+			<div class="text-center">
+				<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Modelagem de banco de dados online</h1>
+				<p class="mt-6 text-lg leading-8 text-gray-600">Crie seus modelos de banco de dados de forma simples e rápida. Com o BRMW você pode criar, editar e exportar SQL direto do seu navegador.</p>
+				<div class="mt-10 flex items-center justify-center gap-x-6">
+					<a href="https://app.brmodeloweb.com/#!/register" class="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Criar conta</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
