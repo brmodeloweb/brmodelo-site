b58e212c08515793522c95039a7211ad716b27b3
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2024-02-18 20:45:41 +0100

    Setup project structure
    - Add Main.svelte
    - Setup page.svelte to load <Main> component
    - Load <Header> via +layout.svelte
    - Load <Footer> via +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
-	import "../app.css";
+	import '../css/colors.css';
+	import '../css/app.css';
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
+<Header />
 <slot />
+<Footer />
