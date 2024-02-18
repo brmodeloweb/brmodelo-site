2867a6f8989d53b607fbc82f1bd1d5c5e626560a
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2024-02-18 20:42:38 +0100

    Add ProjectNumbers.svelte

---
+++ src/components/ProjectNubers.svelte
@@ -0,0 +1,28 @@
+<section id="ProjectNumbers">
+	<div class="bg-white py-24 border-y-2">
+		<div class="mx-auto max-w-6xl px-6">
+			<dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3 pb-16">
+				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
+					<dt class="text-base leading-7 text-gray-600">Usuários</dt>
+					<dd class="order-first text-6xl font-semibold tracking-tight text-gray-900 ">174 mil</dd>
+				</div>
+				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
+					<dt class="text-base leading-7 text-gray-600">Modelos criados</dt>
+					<dd class="order-first text-6xl font-semibold tracking-tight text-gray-900">250 mil</dd>
+				</div>
+				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
+					<dt class="text-base leading-7 text-gray-600">Acessos por mês</dt>
+					<dd class="order-first text-6xl font-semibold tracking-tight text-gray-900">~66 mil</dd>
+				</div>
+			</dl>
+			<div class="flex justify-center text-center">
+				<div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">Você pode ajudar a manter o BRMW no ar. 
+					<a href="#SupportUs" class="font-semibold text-green-600">
+						<span>Saiba mais</span>
+						<span aria-hidden="true">&rarr;</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
