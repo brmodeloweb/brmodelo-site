5e9f26ab80185508d54d18088d30383d6031ab1a
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2024-02-18 20:44:05 +0100

    Add WhoUses.svelte

---
+++ src/components/WhoUse.svelte
@@ -0,0 +1,135 @@
+<script>
+	import feevale from '$lib/img/users/feevale.svg';
+	import iesb from '$lib/img/users/iesb.svg';
+	import ifac from '$lib/img/users/ifac.svg';
+	import ifba from '$lib/img/users/ifba.svg';
+	import iff from '$lib/img/users/iff.svg';
+	import ifms from '$lib/img/users/ifms.svg';
+	import ifpb from '$lib/img/users/ifpb.svg';
+	import ifpr from '$lib/img/users/ifpr.svg';
+	import ifro from '$lib/img/users/ifro.svg';
+	import ifrs from '$lib/img/users/ifrs.svg';
+	import ifsc from '$lib/img/users/ifsc.svg';
+	import ifsp from '$lib/img/users/ifsp.svg';
+	import ufc from '$lib/img/users/ufc.svg';
+	import ufpel from '$lib/img/users/ufpel.svg';
+	import unemat from '$lib/img/users/unemat.svg';
+	import uni_cruzeiro_do_sul from '$lib/img/users/uni_cruzeiro_do_sul.svg';
+	import unicesumar from '$lib/img/users/unicesumar.svg';
+	import unisinos from '$lib/img/users/unisinos.svg';
+	import univille from '$lib/img/users/univille.svg';
+</script>
+
+<section id="WhoUse">
+	<div class="bg-white border-b-2">
+		<div class="mx-auto max-w-7xl px-6 py-24">
+			<h2 class="mb-24 text-3xl font-bold text-center tracking-tight text-gray-900 sm:text-4xl">Quem usa?</h2>
+			<div class="
+				grid
+				grid-cols-6
+				mx-auto
+				max-w-6xl
+				items-center
+				gap-x-8
+				gap-y-12
+				lg:grid-cols-5"
+			>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifac}
+					alt="Instituto Federal do Acre"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifba}
+					alt="Instituto Federal da Bahia"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={iff}
+					alt="Instituto Federal Fluminense"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifms}
+					alt="Instituto Federal do Mato Grosso do Sul"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifpb}
+					alt="Instituto Federal da Paraíba"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifpr}
+					alt="Instituto Federal do Paraná"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifro}
+					alt="Instituto Federal de Rondônia"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifrs}
+					alt="Instituto Federal do Rio Grande do Sul"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifsc}
+					alt="Instituto Federal de Santa Catarina"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifsp}
+					alt="Instituto Federal de São Paulo"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={feevale}
+					alt="Universidade Feevale"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={iesb}
+					alt="Instituto de Educação Superior de Brasília"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ufc}
+					alt="Universidade Federal do Ceará"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ufpel}
+					alt="Universidade Federal de Pelotas"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={unemat}
+					alt="Uneversidade do Estado de Mato Grosso"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={uni_cruzeiro_do_sul}
+					alt="Universidade Cruzeiro do Sul"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={unicesumar}
+					alt="Universidade Cesumar"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={unisinos}
+					alt="Reform"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={univille}
+					alt="Univiile"
+				/>
+			</div>
+		</div>
+	</div>
+</section>
