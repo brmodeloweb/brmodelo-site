338e5c0b2e81d2d03d3ef84d1721935cc624e7fc
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2024-02-27 19:09:47 +0100

    Add more universities
    Based in access data from January to February 2024
    
    - Barão de Mauá
    - Faculdade Unica
    - IFG
    - IFMG
    - UFBA
    - UFMS
    - UFS
    - UFSC
    - UniBrasil
    - Unisal
    - UVV

--- src/components/WhoUse.svelte
+++ src/components/WhoUse.svelte
@@ -1,9 +1,13 @@
 <script>
+	import barao_de_maua from '$lib/img/users/barao_de_maua.svg';
+	import faculdade_unica from '$lib/img/users/faculdade_unica.svg';
 	import feevale from '$lib/img/users/feevale.svg';
 	import iesb from '$lib/img/users/iesb.svg';
 	import ifac from '$lib/img/users/ifac.svg';
 	import ifba from '$lib/img/users/ifba.svg';
 	import iff from '$lib/img/users/iff.svg';
+	import ifg from '$lib/img/users/ifg.svg';
+	import ifmg from '$lib/img/users/ifmg.svg';
 	import ifms from '$lib/img/users/ifms.svg';
 	import ifpb from '$lib/img/users/ifpb.svg';
 	import ifpr from '$lib/img/users/ifpr.svg';
@@ -11,13 +15,20 @@
 	import ifrs from '$lib/img/users/ifrs.svg';
 	import ifsc from '$lib/img/users/ifsc.svg';
 	import ifsp from '$lib/img/users/ifsp.svg';
+	import ufba from '$lib/img/users/ufba.svg';
 	import ufc from '$lib/img/users/ufc.svg';
+	import ufms from '$lib/img/users/ufms.svg';
 	import ufpel from '$lib/img/users/ufpel.svg';
+	import ufs from '$lib/img/users/ufs.svg';
+	import ufsc from '$lib/img/users/ufsc.svg';
 	import unemat from '$lib/img/users/unemat.svg';
 	import uni_cruzeiro_do_sul from '$lib/img/users/uni_cruzeiro_do_sul.svg';
+	import unibrasil from '$lib/img/users/unibrasil.svg';
 	import unicesumar from '$lib/img/users/unicesumar.svg';
+	import unisal from '$lib/img/users/unisal.svg';
 	import unisinos from '$lib/img/users/unisinos.svg';
 	import univille from '$lib/img/users/univille.svg';
+	import uvv from '$lib/img/users/uvv.svg';
 </script>
 
 <section id="WhoUse">
@@ -48,6 +59,16 @@
 					class="col-span-2 w-full object-contain lg:col-span-1"
 					src={iff}
 					alt="Instituto Federal Fluminense"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifg}
+					alt="Instituto Federal de Goiás"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ifmg}
+					alt="Instituto Federal de Minas Gerais"
 				/>
 				<img
 					class="col-span-2 w-full object-contain lg:col-span-1"
@@ -86,6 +107,16 @@
 				/>
 				<img
 					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={barao_de_maua}
+					alt="Centro Universitário Barão de Mauá"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={faculdade_unica}
+					alt="Faculdade Única"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
 					src={feevale}
 					alt="Universidade Feevale"
 				/>
@@ -96,13 +127,33 @@
 				/>
 				<img
 					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ufba}
+					alt="Universidade Federal da Bahia"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
 					src={ufc}
 					alt="Universidade Federal do Ceará"
 				/>
 				<img
 					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ufms}
+					alt="Universidade Federal de Mato Grosso do Sul"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
 					src={ufpel}
 					alt="Universidade Federal de Pelotas"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ufs}
+					alt="Universidade São Francisco"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={ufsc}
+					alt="Universidade Federal de Santa Catarina"
 				/>
 				<img
 					class="col-span-2 w-full object-contain lg:col-span-1"
@@ -116,8 +167,18 @@
 				/>
 				<img
 					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={unibrasil}
+					alt="UniBrasil"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
 					src={unicesumar}
 					alt="Universidade Cesumar"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={unisal}
+					alt="Centro Universitário Salesiano de São Paulo"
 				/>
 				<img
 					class="col-span-2 w-full object-contain lg:col-span-1"
@@ -128,6 +189,11 @@
 					class="col-span-2 w-full object-contain lg:col-span-1"
 					src={univille}
 					alt="Univiile"
+				/>
+				<img
+					class="col-span-2 w-full object-contain lg:col-span-1"
+					src={uvv}
+					alt="Universidade Vila Velha"
 				/>
 			</div>
 		</div>
