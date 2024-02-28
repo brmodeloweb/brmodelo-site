a22a9070fcfc3e000e1d3f4069a9d4195f8d7206
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2024-02-28 18:53:36 +0100

    Use <abbr> to improve accessibility
    - Replace <span> with <abbr>
    - Remove text-decoration
    - Add cursr:help

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -61,7 +61,7 @@
 		<div class="mx-auto max-w-2xl py-32">
 			<div class="text-center">
 				<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Modelagem de banco de dados online</h1>
-				<p class="mt-6 text-lg leading-8 text-gray-700">Crie seus modelos de banco de dados de forma simples e rápida. Com o <span class="inline-block border-2 border-teal-800 rounded px-1 text-sm font-semibold text-teal-800">BRMW</span> você pode criar, editar e exportar SQL direto do seu navegador.</p>
+				<p class="mt-6 text-lg leading-8 text-gray-700">Crie seus modelos de banco de dados de forma simples e rápida. Com o <abbr title="BR Moldelo Web" class="inline-block border-2 border-teal-800 rounded px-1 text-sm font-semibold text-teal-800 no-underline cursor-help">BRMW</abbr> você pode criar, editar e exportar SQL direto do seu navegador.</p>
 				<div class="mt-10 flex items-center justify-center gap-x-6">
 					<a href="https://app.brmodeloweb.com/#!/register" class="rounded-md bg-teal-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Criar conta</a>
 				</div>
