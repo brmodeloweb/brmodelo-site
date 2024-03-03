a7acf324b40bae7e1357b3922041dbc735f70272
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2024-03-03 14:42:38 +0100

    Add missing <h2> to ProjectNumbers.svelte

--- src/components/ProjectNubers.svelte
+++ src/components/ProjectNubers.svelte
@@ -4,6 +4,7 @@
 
 <section id="ProjectNumbers" data-aos="fade-up">
 	<div class="bg-white py-24 border-y-2">
+		<h2 class="sr-only mb-24 text-3xl font-bold text-center tracking-tight text-gray-900 sm:text-4xl">{m.Numbers_title()}</h2>
 		<div class="mx-auto max-w-6xl px-6">
 			<dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
 				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
