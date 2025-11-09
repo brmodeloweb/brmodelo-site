dc6102690401f1b8bf618c3368eccce1d2b9ba2d
Milton Bittencourt <miltonbst@gmail.com>
2025-11-09 09:13:29 -0300

    New build

--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,9 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/brmodelo-site' : ''
+		},
 		alias: {
 			$paraglide: "./src/paraglide",
 		}
