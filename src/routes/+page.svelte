6de505db86e5e51709ff6951527f86d7010d88b8
Milton Bittencourt <miltonbst@gmail.com>
2025-11-09 22:27:16 -0300

    Fix sit url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <svelte:head>
 	<title>BRMW - Modelagem de banco de dados online</title>
-	<meta property="og:url" content="https://brmodeloweb.com"/>
+	<meta property="og:url" content="https://www.brmodeloweb.com"/>
 	<meta property="og:image" content="{OgPreview}"/>
 	<meta property="og:site_name" content="{m.BRMW()}"/>
 	<meta property="og:title" content="{m.title()}"/>
